662772cf6145da90771437ea0a5dfe4fcad32c21
Roman Yaremenko <Romanyaremenko95@gmail.com>
2017-02-25 23:30:21 +0200

    final_4_final

---
+++ Readme.md
@@ -0,0 +1,3 @@
+APK : https://drive.google.com/file/d/0B1W9-47OXzQxR3h2MXJhZ3RsNms/view?usp=sharing
+
+QR : <a href="http://qrcoder.ru" target="_blank"><img src="http://qrcoder.ru/code/?https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F0B1W9-47OXzQxR3h2MXJhZ3RsNms%2Fview%3Fusp%3Dsharing&4&0" width="180" height="180" border="0" title="QR код"></a>
--- app/src/main/java/com/example/roman/final_4/MainActivity.java
+++ app/src/main/java/com/example/roman/final_4/MainActivity.java
@@ -10,9 +10,7 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //Button Btn_Email;
-    //Button Btn_Maps;
-    //Button Btn_Call;
+
     Button Btn_menu;
     ImageButton img_Btn_maps;
     ImageButton img_Btn_call;
@@ -31,17 +29,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-       // Btn_Email = (Button) findViewById(R.id.Btn_Emeil);
-        //Btn_Maps = (Button) findViewById(R.id.Btn_maps);
-        //Btn_Call = (Button) findViewById(R.id.Btn_call);
+
         Btn_menu = (Button) findViewById(R.id.Btn_menu);
         img_Btn_maps = (ImageButton) findViewById(R.id.img_Btn_maps);
         img_Btn_call = (ImageButton) findViewById(R.id.img_Btn_call) ;
         img_Btn_email = (ImageButton) findViewById(R.id.img_Btn_email);
 
-        //Btn_Email.setOnClickListener(this);
-        //Btn_Maps.setOnClickListener(this);
-        //Btn_Call.setOnClickListener(this);
+
         Btn_menu.setOnClickListener(this);
         img_Btn_maps.setOnClickListener(this);
         img_Btn_call.setOnClickListener(this);
@@ -53,24 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClick (View v){
         switch (v.getId()){
-            /*case (R.id.Btn_Emeil):
-                Intent emailIntent = new Intent(Intent.ACTION_SEND);
-                emailIntent.putExtra(Intent.EXTRA_EMAIL,new String[]{Email});
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT,subjeck);
-                emailIntent.putExtra(Intent.EXTRA_TEXT,text);
 
-                emailIntent.setType ("massage/rfc882");
-                startActivity(emailIntent.createChooser(emailIntent,"выберите способ "));
-                break;*/
-            /*case (R.id.Btn_maps):
-                Uri geoUri = Uri.parse(geoUriString);
-                Intent geoIntent = new Intent(Intent.ACTION_VIEW,geoUri);
-                startActivity(geoIntent);
-                break;
-            case (R.id.Btn_call):
-                Intent phoneIntent = new Intent(Intent.ACTION_DIAL,Uri.parse(phoneNub));
-                startActivity(phoneIntent);
-                break;*/
             case  (R.id.Btn_menu):
                 Intent browseIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.lacosanostra.ck.ua/"));
                 startActivity(browseIntent);
